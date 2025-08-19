7b995f4f372acfbd6b6a1e4662dbc4be34bdbbd3
Armeldt <armeldt@outlook.fr>
2025-08-19 18:42:10 +1000

    schema update

--- Schema.sql
+++ Schema.sql
@@ -15,43 +15,48 @@ CREATE TABLE IF NOT EXISTS dim_championships (
 CREATE TABLE IF NOT EXISTS dim_teams (
     team_id TEXT PRIMARY KEY,
     team_name TEXT,
+    team_nickname TEXT,
     team_avatar TEXT,
+    team_faceit_url TEXT,
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS dim_players (
     player_id TEXT PRIMARY KEY,
-    team_id TEXT,
     player_name TEXT,
-    team_id TEXT REFERENCES dim_teams(team_id),
+    team_id TEXT,
     player_country TEXT,
     player_faceit_url TEXT,
+    player_avatar TEXT,
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS fact_matches (
-match_id TEXT,
-match_round TEXT,
-competition_id TEXT REFERENCES dim_championships(competition_id),
-winner_id TEXT REFERENCES dim_teams(team_id),
-total_rounds_played INT,
-PRIMARY KEY (match_id, match_round),
-created_at TIMESTAMPTZ DEFAULT NOW()
+CREATE TABLE IF NOT EXISTS dim_matches (
+    match_id TEXT,
+    match_round TEXT,
+    competition_id TEXT,
+    match_day DATE,
+    format TEXT,
+    map TEXT,
+    total_rounds_played INT,
+    winner_id TEXT,
+    loser_id TEXT,
+    PRIMARY KEY (match_id, match_round),
+    created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS fact_teams_stats (
     match_id TEXT,
     match_round TEXT,
-    team_id TEXT REFERENCES dim_teams(team_id),
+    team_id TEXT,
+    competition_id TEXT,
     team_win INT,
-    total_rounds_played INT,
+    total_rounds_played TEXT,
     first_half_score INT,
     second_half_score INT,
     overtime_score INT,
     final_score INT,
     created_at TIMESTAMPTZ DEFAULT NOW(),
-    FOREIGN KEY (match_id, match_round) 
-        REFERENCES fact_matches(match_id, match_round),
     PRIMARY KEY (match_id, match_round, team_id)
 );
 
@@ -59,36 +64,37 @@ CREATE TABLE IF NOT EXISTS fact_players_stats (
     match_id TEXT,
     match_round TEXT,
     player_id TEXT,
-    zeuskills INT,
-    utilitycount INT,
-    doublekills INT,
-    flashsuccesses INT,
-    quadrokills INT,
-    entrycount INT,
-    firstkills INT,
-    flashcount INT,
-    sniperkills INT,
+    competition_id TEXT,
+    team_id TEXT,
+    zeus_kills INT,
+    utility_count INT,
+    double_kills INT,
+    flash_successes INT,
+    quadro_kills INT,
+    entry_count INT,
+    first_kills INT,
+    flash_count INT,
+    sniper_kills INT,
     damage INT,
-    utilitydamage INT,
+    utility_damage INT,
     assists INT,
     count_1v1 INT,
-    enemiesflashed INT,
-    clutchkills INT,
-    pentakills INT,
+    enemies_flashed INT,
+    clutch_kills INT,
+    ace INT,
     mvps INT,
     deaths INT,
-    entrywins INT,
+    entry_wins INT,
     kills INT,
     wins_1v2 INT,
     wins_1v1 INT,
-    pistolkills INT,
-    knifekills INT,
+    pistol_kills INT,
+    knife_kills INT,
     adr INT,
     count_1v2 INT,
-    triplekills INT,
+    triple_kills INT,
     headshots INT,
+    started_at DATE,
     created_at TIMESTAMPTZ DEFAULT NOW(),
-    FOREIGN KEY (match_id, match_round) 
-        REFERENCES fact_matches(match_id, match_round),
     PRIMARY KEY (match_id, match_round, player_id)
 )
